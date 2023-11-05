231b54f1a5a1140ab59712bc291a8a76d872988d
Sol Eugenia Perez Sanchez <Solperezsanchez03@gmail.com>
2023-11-05 12:20:31 -0300

    .

--- src/components/Carrito.jsx
+++ src/components/Carrito.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import { CartContext } from '../context/CartContext'
 import { Link } from 'react-router-dom';
 
+
 const Carrito = () => {
 
     const { carrito, precioTotal, vaciarCarrito } = useContext(CartContext);
